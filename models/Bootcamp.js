3ccc7cdc87860fbdba2b4b1260f55217d765c988
Rasmus Rajasalo <rarasmus@outlook.com>
2020-02-13 15:40:37 +0200

    Create errorHandler middleware and utils dir with ErrorResponse class

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -41,11 +41,11 @@ const BootcampSchema = new mongoose.Schema({
     type: {
       type: String,
       enum: ['Point'],
-      required: true
+      required: false // did not work with true
     },
     coordinates: {
       type: [Number],
-      required: true,
+      required: false, // did not work with true
       index: '2dsphere'
     },
     formattedAddress: String,
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const morgan = require('morgan');
 const colors = require('colors');
 // const logger = require('./middleware/logger');
+const errorHandler = require('./middleware/error');
 const connectDB = require('./config/db');
 
 // Load env vars
@@ -16,6 +17,9 @@ const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
+// Body parser
+app.use(express.json());
+
 // Dev logging middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -25,6 +29,8 @@ if (process.env.NODE_ENV === 'development') {
 
 // Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
